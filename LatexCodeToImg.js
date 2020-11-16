bf0d42f1691f6405e0541694e8bb113c4fc59ab7
Kevin-Beta <p.body@gmail.com>
2020-11-16 12:17:11 +0800

    完善LatexCodeToImg.js界面

--- LatexCodeToImg.js
+++ LatexCodeToImg.js
@@ -1,5 +1,8 @@
 let suckIt = `
 <div style="position: fixed;top: 50%;z-index: 999;right: 10%;padding: 1em;background: rgba(233,233,233,.6);border: 1px solid #aaa;">
+
+	公式图像:(转换时间1~3秒)
+	<br>
 	<img src="http://latex.codecogs.com/gif.latex?x+y=z" id="img" />
 	<br>
 	<textarea placeholder="LatexCode" id="code" style="padding: 10px;height: 20px;width: 200px;margin: 10px;">x+y=z</textarea>
@@ -18,5 +21,5 @@ function convert() {
 		let url="http://latex.codecogs.com/gif.latex?"+code;
 		document.getElementById('img').src=url;
 		window.clearInterval(fuck);
-	}, 2333);
+	}, 10);
 }
